56561efe92da63a27c188aa2943ecaef6e60cd02
luzagm <luz.aguirre.mo@gmail.com>
2019-11-26 08:12:47 +0100

    add localStorage

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # TO DO LIST
+
+SPA created with Vanilla JavaScript for Adalab (Week #6 of online training).
+
+## Requirements
+
+[x] This test will be in plain JavaScript (no frameworks used).
+
+[ ] The main priority will be to complete tasks.
+
+[ ] The next priority, once done the previous one, is approaching the best you can to the provided sketch.
+
+[ ] Is mandatory to use Local Storage functionality.
+
+[ ] Write today's date!
+
+[x] Everything will be in just one page.
+
+
+## Tasks
+
+[ ] Complete a task: When you click on a task or its checkbox, it checks the task as done and put its at the end of the list.
+
+[ ] Uncheck a finished task: Click on a completed task or its checkbox, it checks the task as pending and put its at the top of the list.
+
+[ ] Add a new task: It shows a layout where you can write the name of the task and add it. It will show up at the top of the list.
--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,38 @@
-'use strict';
+"use strict";
 
-console.log('>> Ready :)');
+const openModalBtn = document.getElementById("modal-trigger");
+const modal = document.getElementById("myModal");
+const addBtn = document.querySelector(".js-addBtn");
+const inputTask = document.querySelector(".js-input");
+
+let newTasks = [];
+
+const getTasksFromLocalStorage = () => {
+  JSON.parse(localStorage.getItem("task"));
+};
+
+const setTasksIntoLocalStorage = () => {
+  localStorage.setItem("task", JSON.stringify(newTasks));
+};
+
+function openModalTask() {
+  modal.classList.remove("is-hidden");
+}
+
+function closeModalTask() {
+  modal.classList.add("is-hidden");
+}
+
+function addTask(ev) {
+  ev.preventDefault();
+  modal.classList.add("is-hidden");
+  const list = document.querySelector(".js-list");
+  const taskName = document.querySelector(".js-input").value;
+  newTasks.push(taskName);
+  list.innerHTML += `<li class="list__item"><input id="checkbox" type="checkbox" /><p id="task-text">${taskName}</p></li>`;
+  setTasksIntoLocalStorage();
+}
+
+getTasksFromLocalStorage();
+openModalBtn.addEventListener("click", openModalTask);
+addBtn.addEventListener("click", addTask);
